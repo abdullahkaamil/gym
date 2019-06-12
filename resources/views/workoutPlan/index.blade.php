1bbc487dcb42cac98c9437263e5ba818152adef2
digitaltradeYazilim <yazilim@digitaltrade.com.tr>
2019-06-12 15:09:33 +0200

    adding the select option , and dynamic box adding

--- resources/views/workoutPlan/index.blade.php
+++ resources/views/workoutPlan/index.blade.php
@@ -10,24 +10,50 @@
                 <div class="row">
         <div class="panel-body bg-white">
         <form action="" method="post">
-                <div class="form-group">
-                    <label for="formGroupday1Input">Day1:</label>
-                    <textarea class="form-control" rows="5" cols="80" name="cmd"></textarea>
-                </div>
-                
-                <div class="form-group">
-                    <label for="formGroupday2Input">Day2:</label>
-                    <textarea class="form-control" rows="5" cols="80" name="cmd"></textarea>
-                </div>
+                <div>
+                        <label for="formGroupday1Input">Select Days for Workout</label>
+                        <select name="day" id="" class="form-control" >
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                        </select>
+                    </div>
 
-                <div class="form-group">
-                    <label for="formGroupday3Input">Day3:</label>
-                    <textarea class="form-control" rows="5" cols="80" name="cmd"></textarea>
-                </div>
             </form>
+            <a type="button" id="add" class="btn btn-info">Add Day</a>
         </div>  
     </div> 
 </div>  
 </div>
 
+
+
+
+
 @endsection
+
+@section('footer_script_init')
+<script type="text/javascript">
+    $('#add').on('click',function(){
+        var c = $('select').val();
+        for(i=0; i<c; i++){
+            addRow(i);
+        }
+    });
+    
+    function addRow(i){
+        var div =  '<div class="form-group">'+
+                    '<label for="formGroupday1Input">Day '+(i+1)+':</label>'+
+                    '<textarea class="form-control" rows="5" cols="80" name="'+(i+1)+'"></textarea>'+
+                    '</div>';
+                    $('form').append(div);
+    }
+
+    
+    </script>    
+
+@stop
